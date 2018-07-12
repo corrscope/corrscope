e0bcd53be4c0607fbb8727307500ef63ea0c64dc
nyanpasu64 <nyanpasu64@tuta.io>
2018-07-12 15:27:26 -0700

    initial code

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='ovgenpy',
+    version='0',
+    packages=[''],
+    url='',
+    license='BSD-2-Clause',
+    author='nyanpasu64',
+    author_email='',
+    description='',
+    install_requires=['numpy', 'scipy', 'imageio', 'click']     # 'pyqt5'
+)
