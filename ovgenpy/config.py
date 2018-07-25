8a08b136d55801254f01b2eca4d724035fe9dec6
jimbo1qaz <jimbo1qaz@gmail.com>
2018-07-25 05:06:12 -0700

    Exclude default fields from YAML dumping. Add dumping unit tests.

--- ovgenpy/config.py
+++ ovgenpy/config.py
@@ -1,20 +1,57 @@
-from dataclasses import dataclass
-from ruamel.yaml import YAML, yaml_object
+from io import StringIO
+
+from dataclasses import dataclass, fields
+from ruamel.yaml import yaml_object, YAML
 
 
-# typing.NamedTuple is incompatible with yaml.register_class.
-# dataclasses.dataclass is compatible.
-# So use the latter.
+class MyYAML(YAML):
+    def dump(self, data, stream=None, **kw):
+        inefficient = False
+        if stream is None:
+            inefficient = True
+            stream = StringIO()
+        YAML.dump(self, data, stream, **kw)
+        if inefficient:
+            return stream.getvalue()
 
-# __init__-less classes are also compatible with yaml.register_class.
+
+# https://yaml.readthedocs.io/en/latest/dumpcls.html
+# >Only yaml = YAML(typ='unsafe') loads and dumps Python objects out-of-the-box. And
+# >since it loads any Python object, this can be unsafe.
+# I assume roundtrip is safe.
+yaml = MyYAML()
 
 
-yaml = YAML()
+def __getstate__(self):
+    state = {}
+    cls = type(self)
+
+    for field in fields(self):
+        name = field.name
+        value = getattr(self, name)
+        default = getattr(cls, name, object())
+
+        if value != default:
+            state[name] = value
+
+    return state
 
 
 def register_config(cls):
+    """ Marks class as @dataclass, and enables YAML dumping (excludes default fields).
+
+    dataclasses.dataclass is compatible with yaml.register_class.
+    typing.NamedTuple is incompatible.
+    """
+
+    # SafeRepresenter.represent_yaml_object() uses __getstate__ to dump objects.
+    cls.__getstate__ = __getstate__
+
     # https://stackoverflow.com/a/51497219/2683842
-    # YAML.register_class(cls) has only returned cls since 2018-07-12.
+    # YAML().register_class(cls) works... on versions more recent than 2018-07-12.
     return yaml_object(yaml)(
         dataclass(cls)
     )
+
+
+# __init__-less non-dataclasses are also compatible with yaml.register_class.
