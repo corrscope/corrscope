1834361d6973d432b0a282ca29ec0279cc46943a
jimbo1qaz <jimbo1qaz@gmail.com>
2018-11-17 01:13:46 -0800

    Add separate trigger and render_subsampling configs

--- tests/test_channel.py
+++ tests/test_channel.py
@@ -1,7 +1,9 @@
+from typing import Optional
+
 import numpy as np
 import pytest
-from hypothesis import given, reproduce_failure
-from hypothesis.strategies import integers
+from hypothesis import given
+import hypothesis.strategies as hs
 from pytest_mock import MockFixture
 
 import ovgenpy.channel
@@ -9,21 +11,25 @@ import ovgenpy.ovgenpy
 from ovgenpy.channel import ChannelConfig, Channel
 from ovgenpy.ovgenpy import default_config, Ovgen, BenchmarkMode
 from ovgenpy.triggers import NullTriggerConfig
+from ovgenpy.util import coalesce
 
-assert reproduce_failure
+positive = hs.integers(min_value=1, max_value=100)
+maybe = hs.one_of(positive, hs.none())
 
-
-positive = integers(min_value=1, max_value=100)
-
-@given(subsampling=positive, trigger_width=positive, render_width=positive)
+@given(subsampling=positive, tsub=maybe, rsub=maybe,
+       trigger_width=positive, render_width=positive)
 def test_channel_subsampling(
-    subsampling: int,
-    trigger_width: int,
-    render_width: int,
-    mocker: MockFixture
+        subsampling: int,
+        tsub: Optional[int],
+        rsub: Optional[int],
+        trigger_width: int,
+        render_width: int,
+        mocker: MockFixture
 ):
-    """ Ensure window_samp and trigger/render subsampling are computed correctly. """
+    """ Ensure trigger/render_samp and trigger/render subsampling
+    are computed correctly. """
 
+    # region setup test variables
     ovgenpy.ovgenpy.PRINT_TIMESTAMP = False    # Cleanup Hypothesis testing logs
 
     Wave = mocker.patch.object(ovgenpy.channel, 'Wave')
@@ -44,39 +50,50 @@ def test_channel_subsampling(
     cfg = default_config(
         channels=[ccfg],
         subsampling=subsampling,
+        trigger_subsampling=tsub,
+        render_subsampling=rsub,
         trigger=NullTriggerConfig(),
         benchmark_mode=BenchmarkMode.OUTPUT
     )
     channel = Channel(ccfg, cfg)
+    # endregion
 
     # Ensure channel.window_samp, trigger_subsampling, render_subsampling are correct.
-    ideal_nsamp = pytest.approx(
-        round(cfg.render_width_s * channel.wave.smp_s / subsampling), 1)
+    tsub = coalesce(tsub, subsampling)
+    rsub = coalesce(rsub, subsampling)
 
-    assert channel.window_samp == ideal_nsamp
-    assert channel.trigger_subsampling == subsampling * trigger_width
-    assert channel.render_subsampling == subsampling * render_width
+    def ideal_samp(sub):
+        return pytest.approx(
+            round(cfg.width_s * channel.wave.smp_s / sub), abs=1)
 
-    # Ensure trigger uses channel.window_samp and trigger_subsampling.
+    ideal_tsamp = ideal_samp(tsub)
+    ideal_rsamp = ideal_samp(rsub)
+    assert channel.render_samp == ideal_rsamp
+    del subsampling
+
+    assert channel.trigger_stride == tsub * trigger_width
+    assert channel.render_stride == rsub * render_width
+
+    ## Ensure trigger uses channel.window_samp and trigger_stride.
     trigger = channel.trigger
-    assert trigger._tsamp == channel.window_samp
-    assert trigger._subsampling == channel.trigger_subsampling
+    assert trigger._tsamp == ideal_tsamp
+    assert trigger._subsampling == channel.trigger_stride
 
-    # Ensure ovgenpy calls render using channel.window_samp and render_subsampling.
+    ## Ensure ovgenpy calls render using channel.render_samp and render_stride.
     ovgen = Ovgen(cfg, '.', outputs=[])
     renderer = mocker.patch.object(Ovgen, '_load_renderer').return_value
     ovgen.play()
 
-    # Inspect arguments to wave.get_around()
+    # Only render (not NullTrigger) calls wave.get_around().
     (_sample, _region_nsamp, _subsampling), kwargs = wave.get_around.call_args
-    assert _region_nsamp == channel.window_samp
-    assert _subsampling == channel.render_subsampling
+    assert _region_nsamp == channel.render_samp
+    assert _subsampling == channel.render_stride
 
     # Inspect arguments to renderer.render_frame()
     # datas: List[np.ndarray]
     (datas,), kwargs = renderer.render_frame.call_args
     render_data = datas[0]
-    assert len(render_data) == channel.window_samp
+    assert len(render_data) == channel.render_samp
 
 
 # line_color is tested in test_renderer.py
