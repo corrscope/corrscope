d5ff171f16156b4085d8317e19aa47078823e314
jimbo1qaz <jimbo1qaz@gmail.com>
2018-12-05 07:47:28 -0800

    Improve test for config model, add trigger/render/width_ms

--- tests/test_channel.py
+++ tests/test_channel.py
@@ -9,37 +9,49 @@ from pytest_mock import MockFixture
 import ovgenpy.channel
 import ovgenpy.ovgenpy
 from ovgenpy.channel import ChannelConfig, Channel
+from ovgenpy.config import OvgenError
 from ovgenpy.ovgenpy import default_config, Ovgen, BenchmarkMode
 from ovgenpy.triggers import NullTriggerConfig
 from ovgenpy.util import coalesce
 
+
 positive = hs.integers(min_value=1, max_value=100)
+Positive = int
 
 # In order to get good shrinking behaviour, try to put simpler strategies first.
 maybe = hs.one_of(hs.none(), positive)
+Maybe = Optional[int]
+
 
 @pytest.mark.filterwarnings("ignore::ovgenpy.config.OvgenWarning")
-@given(subsampling=positive, tsub=maybe, rsub=maybe,
-       g_trigger_width=positive, g_render_width=positive,
-       c_trigger_width=maybe, c_render_width=maybe,
-       )
-def test_channel_subsampling(
-        # Global
-        subsampling: int,
-        tsub: Optional[int],
-        rsub: Optional[int],
+@given(
+    # Channel
+    c_trigger_width=maybe, c_render_width=maybe,
 
-        g_trigger_width: int,
-        g_render_width: int,
+    # Global
+    width_ms=maybe, trigger_ms=maybe, render_ms=maybe,
+    subsampling=positive, tsub=maybe, rsub=maybe,
+    g_trigger_width=positive, g_render_width=positive,
 
-        # Channel
-        c_trigger_width: Optional[int],
-        c_render_width: Optional[int],
+)
+def test_config_channel_width_stride(
+    # Channel
+    c_trigger_width: Maybe, c_render_width: Maybe,
 
-        mocker: MockFixture
+    # Global
+    width_ms: Maybe, trigger_ms: Maybe, render_ms: Maybe,
+    subsampling: Positive, tsub: Maybe, rsub: Maybe,
+    g_trigger_width: Positive, g_render_width: Positive,
+
+    mocker: MockFixture,
 ):
-    """ Ensure trigger/render_samp and trigger/render subsampling
-    are computed correctly. """
+    """ (Tautologically) verify:
+    -     cfg.t/r_ms (given width_ms)
+    - channel.  r_samp (given cfg)
+    - channel.t/r_stride (given cfg.sub/width and cfg.width)
+    - trigger._tsamp, _stride
+    - renderer's method calls(samp, stride)
+    """
 
     # region setup test variables
     ovgenpy.ovgenpy.PRINT_TIMESTAMP = False    # Cleanup Hypothesis testing logs
@@ -59,20 +71,36 @@ def test_channel_subsampling(
         trigger_width=c_trigger_width,
         render_width=c_render_width,
     )
-    cfg = default_config(
-        channels=[ccfg],
-        subsampling=subsampling,
-        trigger_subsampling=tsub,
-        render_subsampling=rsub,
+    def get_cfg():
+        return default_config(
+            width_ms=width_ms,
+            trigger_ms=trigger_ms,
+            render_ms=render_ms,
 
-        trigger_width=g_trigger_width,
-        render_width=g_render_width,
+            subsampling=subsampling,
+            trigger_subsampling=tsub,
+            render_subsampling=rsub,
 
-        trigger=NullTriggerConfig(),
-        benchmark_mode=BenchmarkMode.OUTPUT
-    )
-    channel = Channel(ccfg, cfg)
+            trigger_width=g_trigger_width,
+            render_width=g_render_width,
+
+            channels=[ccfg],
+            trigger=NullTriggerConfig(),
+            benchmark_mode=BenchmarkMode.OUTPUT
+        )
     # endregion
+
+    if not (width_ms or (trigger_ms and render_ms)):
+        with pytest.raises(OvgenError):
+            _cfg = get_cfg()
+        return
+
+    cfg = get_cfg()
+    channel = Channel(ccfg, cfg)
+
+    # Ensure cfg.width_ms etc. are correct
+    assert cfg.trigger_ms == coalesce(trigger_ms, width_ms)
+    assert cfg.render_ms == coalesce(render_ms, width_ms)
 
     # Ensure channel.window_samp, trigger_subsampling, render_subsampling are correct.
     tsub = coalesce(tsub, subsampling)
@@ -81,7 +109,7 @@ def test_channel_subsampling(
     def ideal_samp(width_ms, sub):
         width_s = width_ms / 1000
         return pytest.approx(
-            round(width_s * channel.wave.smp_s / sub), abs=1)
+            round(width_s * channel.wave.smp_s / sub), rel=1e-6)
 
     ideal_tsamp = ideal_samp(cfg.trigger_ms, tsub)
     ideal_rsamp = ideal_samp(cfg.render_ms, rsub)
