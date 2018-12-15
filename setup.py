021e1578942d071433dfd22e4cb9bf2c7da12d30
nyanpasu64 <nyanpasu64@tuta.io>
2018-12-14 23:14:32 -0800

    Update ruamel.yaml to 0.15.70, add behavior tests

--- setup.py
+++ setup.py
@@ -11,8 +11,8 @@ setup(
     description='',
     tests_require=['pytest>=3.2.0', 'pytest-pycharm', 'hypothesis', 'delayed-assert'],
     install_requires=[
-        # ruamel.yaml 0.15.55 implements slice assignment in loaded list (CommentedSeq).
-        'numpy', 'scipy', 'click', 'ruamel.yaml>=0.15.55', 'more_itertools',
+        'ruamel.yaml>=0.15.70',  # See test_config.py to pick a suitable minimum version
+        'numpy', 'scipy', 'click', 'more_itertools',
         'matplotlib',
         'attrs>=18.2.0',
         'PyQt5',
