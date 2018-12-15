6d60803d85a07000f652210f9901258a1fab4610
nyanpasu64 <nyanpasu64@tuta.io>
2018-12-14 20:58:00 -0800

    Update ruamel.yaml to fix GUI errors (reordering channels)

--- setup.py
+++ setup.py
@@ -11,7 +11,8 @@ setup(
     description='',
     tests_require=['pytest>=3.2.0', 'pytest-pycharm', 'hypothesis', 'delayed-assert'],
     install_requires=[
-        'numpy', 'scipy', 'click', 'ruamel.yaml', 'more_itertools',
+        # ruamel.yaml 0.15.55 implements slice assignment in loaded list (CommentedSeq).
+        'numpy', 'scipy', 'click', 'ruamel.yaml>=0.15.55', 'more_itertools',
         'matplotlib',
         'attrs>=18.2.0',
         'PyQt5',
