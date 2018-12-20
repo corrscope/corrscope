f86e62130802c7400669db7e838e0907ea8d2758
nyanpasu64 <nyanpasu64@tuta.io>
2018-12-20 02:19:28 -0800

    Edit setup.py

--- setup.py
+++ setup.py
@@ -3,12 +3,14 @@ from setuptools import setup
 setup(
     name='ovgenpy',
     version='0',
-    packages=[''],
-    url='',
+    packages=['ovgenpy'],
+    url='https://github.com/nyanpasu64/ovgenpy',
     license='BSD-2-Clause',
     author='nyanpasu64',
     author_email='',
     description='',
+    # https://docs.pytest.org/en/latest/goodpractices.html
+    setup_requires=["pytest-runner"],
     tests_require=['pytest>=3.2.0', 'pytest-pycharm', 'hypothesis', 'delayed-assert'],
     install_requires=[
         'ruamel.yaml>=0.15.70',  # See test_config.py to pick a suitable minimum version
