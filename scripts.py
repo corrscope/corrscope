17cb469313bc3b03c6df3f4c19a8814a98f415f6
jimbo1qaz <jimbo1qaz@gmail.com>
2019-01-12 02:44:51 -0800

    Fix previous PR, add `poetry run` coverage scripts

---
+++ scripts.py
@@ -0,0 +1,52 @@
+""" poetry run func_name """
+import shlex
+import webbrowser
+
+# Obtain path from package.dist-info/entry_points.txt
+def run(path, arg_str):
+    module, func = path.split(":")
+    argv = shlex.split(arg_str)
+    exec(
+        f"""\
+from {module} import {func}
+{func}({argv})
+"""
+    )
+
+
+# public:
+def dcover():
+    """ Run coverage and diff-cover."""
+    cover()
+    diff()
+
+
+def rcover():
+    """ Run coverage and report. """
+    cover()
+    report()
+
+
+def hcover():
+    """ Run coverage and open HTML. """
+    cover()
+    html()
+
+
+# public helpers:
+def cover():
+    run("pytest:main", "--tb=short --cov=corrscope")
+
+
+def diff():
+    # argv[0:]
+    run("diff_cover.tool:main", "diff-cover coverage.xml")
+
+
+def report():
+    run("coverage.cmdline:main", "report")
+
+
+def html():
+    run("coverage.cmdline:main", "html")
+    webbrowser.open("htmlcov/index.html")
