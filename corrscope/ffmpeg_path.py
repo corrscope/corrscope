265594f4b5ff0610e317c127a14372b072e75869
nyanpasu64 <nyanpasu64@tuta.io>
2019-01-16 03:48:10 -0800

    Prepend (don't append) app-specific dir to PATH

--- corrscope/ffmpeg_path.py
+++ corrscope/ffmpeg_path.py
@@ -1,6 +1,7 @@
 import os
 import platform
 import sys
+from typing import Dict, List
 from pathlib import Path
 
 from appdirs import user_data_dir
@@ -12,6 +13,12 @@ from corrscope.config import CorrError
 __all__ = ["data_dir", "path_dir", "get_ffmpeg_url", "MissingFFmpegError"]
 
 
+def prepend(dic: Dict[str, str], _key: List[str], prefix: str) -> None:
+    """ Dubiously readable syntactic sugar for prepending to a string in a dict. """
+    key = _key[0]
+    dic[key] = prefix + dic[key]
+
+
 data_dir = Path(user_data_dir(corrscope.app_name, appauthor=False, roaming=True))
 data_dir.mkdir(exist_ok=True)
 
@@ -20,7 +27,7 @@ _path_dir = data_dir / "path"
 _path_dir.mkdir(exist_ok=True)
 
 path_dir = str(_path_dir)
-os.environ["PATH"] += os.pathsep + path_dir
+prepend(os.environ, ["PATH"], path_dir + os.pathsep)
 # Editing sys.path doesn't work.
 # https://bugs.python.org/issue8557 is relevant but may be outdated.
 
