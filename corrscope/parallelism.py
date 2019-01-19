8bae456a6123919b80f9fb0bea8d449f0ba9f349
jimbo1qaz <jimbo1qaz@gmail.com>
2019-01-19 00:01:38 -0800

    parallelism debug prints

--- corrscope/parallelism.py
+++ corrscope/parallelism.py
@@ -32,6 +32,7 @@ frame 1:
 frame:
 - parent reads and quits
 """
+import datetime
 from abc import ABC, abstractmethod
 from enum import Enum, auto
 from multiprocessing import Process, Pipe
@@ -39,6 +40,8 @@ from multiprocessing.connection import Connection
 from typing import *
 
 import numpy as np
+
+from corrscope.util import perr
 
 __all__ = [
     "Message",
@@ -134,6 +137,7 @@ class ParallelWorker(Worker):
         # Receive reply from child.
         if self._not_first:
             is_aborted_or_error = self._parent_conn.recv()  # type: ReplyOrError
+            perr(datetime.datetime.now(), "parent recv", str(is_aborted_or_error)[:10])
             if is_aborted_or_error is not False:
                 self._child_dead = True
 
@@ -146,6 +150,7 @@ class ParallelWorker(Worker):
                 pass
 
         # Send parent message.
+        perr(datetime.datetime.now(), "parent to child", str(msg)[:10])
         self._parent_conn.send(msg)
         self._not_first = True
 
@@ -160,6 +165,7 @@ class ParallelWorker(Worker):
             while True:
                 # Receive parent message.
                 msg = child_conn.recv()
+                perr(datetime.datetime.now(), "child recv", str(msg)[:10])
                 if msg is None:
                     break  # See module docstring
 
@@ -167,9 +173,11 @@ class ParallelWorker(Worker):
                 try:
                     reply = child_job.foreach(msg)
                 except BaseException as e:
+                    perr(datetime.datetime.now(), "child Error")
                     child_conn.send(Error.Error)
                     raise
                 else:
+                    perr(datetime.datetime.now(), "child to parent", str(reply)[:10])
                     child_conn.send(reply)
                     if reply:
                         break
