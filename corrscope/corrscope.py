f0110df56b3015f846bece971761c089e19ba7fc
jimbo1qaz <jimbo1qaz@gmail.com>
2019-06-02 11:28:26 -0700

    Always enable midline color, never None (#291)
    Previously, midline_color was None by default but #404040 in the template.
    Now it's #404040 by default, and changed from Optional[str] to str.
    
    Eliminates bug where unchecking "midline color" (midline_color=None)
    switches to blue, instead of transparent.

--- corrscope/corrscope.py
+++ corrscope/corrscope.py
@@ -122,12 +122,7 @@ def default_config(**kwargs) -> Config:
         channels=[],
         layout=LayoutConfig(orientation="v", ncols=1),
         render=RendererConfig(
-            1280,
-            720,
-            res_divisor=4 / 3,
-            midline_color="#404040",
-            v_midline=True,
-            h_midline=True,
+            1280, 720, res_divisor=4 / 3, v_midline=True, h_midline=True
         ),
     )
     return attr.evolve(cfg, **kwargs)
