1d370e65015bd0484cd545edd59f2bce75b5c5da
nyanpasu64 <nyanpasu64@tuta.io>
2020-04-25 20:30:46 -0700

    Switch to static ffmpeg 4.2.2 for reliability (#332)
    Previously we linked to shared-dll ffmpeg builds.
    When downloading the shared builds, some users fail to extract the DLLs,
    causing ffmpeg to not work.
    Static has a larger filesize, but is less likely to confuse users.
    
    Previously, we linked to the latest ffmpeg dev build.
    I'm unsure if ffmpeg dev builds are unstable
    or may have bugs varying from version to version.
    And there is no URL on zeranoe's website to get the latest release build.
    So instead hard-code/pin the current latest release,
    and update it from time to time.

--- corrscope/settings/paths.py
+++ corrscope/settings/paths.py
@@ -33,11 +33,13 @@ prepend(os.environ, ["PATH"], PATH_dir + os.pathsep)
 
 
 def get_ffmpeg_url() -> str:
+    linking = "static"
+
     # is_python_64 = sys.maxsize > 2 ** 32
     is_os_64 = platform.machine().endswith("64")
 
     def url(os_ver: str) -> str:
-        return f"https://ffmpeg.zeranoe.com/builds/{os_ver}/shared/ffmpeg-latest-{os_ver}-shared.zip"
+        return f"https://ffmpeg.zeranoe.com/builds/{os_ver}/{linking}/ffmpeg-4.2.2-{os_ver}-{linking}.zip"
 
     if sys.platform == "win32" and is_os_64:
         return url("win64")
