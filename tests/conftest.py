28802c8c8cac58207f65df97602c696c2c17ec82
nyanpasu64 <nyanpasu64@tuta.io>
2025-12-16 14:35:54 -0800

    Rewrite mock Popen in unit tests for ffprobe compatibility
    The most principled way to do this is to pass file/process open
    capabilities into CorrScope, and pass in mocks during unit tests. I
    don't want to rewrite the code to make it happen.

--- tests/conftest.py
+++ tests/conftest.py
@@ -32,33 +32,39 @@ def Popen(mocker: "pytest_mock.MockFixture"):
       and give its classmethods to Popen.
     """
     real_Popen = subprocess.Popen
+    exception_on_write: Optional[Exception] = None
 
-    class MockPopen(real_Popen):
-        exception_on_write: Optional[Exception] = None
+    def set_exception(exc: Exception):
+        nonlocal exception_on_write
+        exception_on_write = exc
 
-        @classmethod
-        def set_exception(cls, exc: Exception):
-            cls.exception_on_write = exc
+    def new_popen(*args, **kwargs):
+        popen = mocker.create_autospec(real_Popen)
 
-        @classmethod
-        def __new__(cls, *args, **kwargs):
-            popen = mocker.create_autospec(real_Popen)
+        popen.stdin = mocker.mock_open()(os.devnull, "wb")
+        popen.stdout = mocker.mock_open()(os.devnull, "rb")
+        assert popen.stdin != popen.stdout
 
-            popen.stdin = mocker.mock_open()(os.devnull, "wb")
-            popen.stdout = mocker.mock_open()(os.devnull, "rb")
-            assert popen.stdin != popen.stdout
+        if exception_on_write is not None:
+            popen.stdin.write.side_effect = exception_on_write
 
-            if cls.exception_on_write is not None:
-                popen.stdin.write.side_effect = cls.exception_on_write
-
-            popen.wait.return_value = 0
-            return popen
+        popen.wait.return_value = 0
+        return popen
 
     # Popen acts exactly the same as MockPopen
     # when calling classmethods or creating instances.
     # But Popen is a MagicMock, and captures Popen() call arguments.
 
     Popen = mocker.patch.object(subprocess, "Popen", autospec=True)
-    Popen.set_exception = MockPopen.set_exception
-    Popen.side_effect = MockPopen.__new__
+    Popen.set_exception = set_exception
+    Popen.side_effect = new_popen
+
+    # ffprobe_is_mono() calls subprocess.run().
+    run = mocker.patch.object(subprocess, "run", autospec=True)
+
+    def do_run(args, *pos, **kwargs):
+        return subprocess.CompletedProcess(args, 0, b"1\n", b"")
+
+    run.side_effect = do_run
+
     yield Popen
